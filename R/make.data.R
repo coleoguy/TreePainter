ea622e6022bfcde6711aaad38bb6a2c24625e249
Terrence Sylvester <52291097+Tsylvester8@users.noreply.github.com>
2021-03-23 22:30:01 -0500

    added library(geiger)

--- R/make.data.R
+++ R/make.data.R
@@ -14,17 +14,17 @@ for(i in 1:100){
                                model = "mkn",x0 = 2)
 }
 library(castor)
+library(geiger)
 
-
-fit3 <- treePaintR(tree = trees[[1]],
+fit <- treePaintR(tree = trees[[1]],
                   tip_states = traits[[1]],
                   qmat = qmat,
                   iter = 10000,
-                  rate.classes = 13,
-                  step = 9)
+                  rate.classes = 21,
+                  step = .1)
 plot(tree = fit[[1]],
-     rates = 13,
-     scaled = T,
+     rates = 21,
+     scaled = F,
      cols=NULL,
      bg="lightgray", edge.width = 1)
 plot(tree = fit2[[1]],
@@ -38,9 +38,10 @@ plot(tree = fit3[[1]],
      cols=NULL,
      bg="lightgray", edge.width = 1)
 
-plot(fit2$lk.trace, type = "l",
+plot(fit$lk.trace, type = "l",
      ylab = "lnLik",
-     xlab = "generation")
+     xlab = "generation",
+     col = heat.colors(3)[as.factor(1,2,3)])
 lines(fit$lk.trace, type = "l",col="red")
 lines(fit3$lk.trace, type = "l",col="red")
 
