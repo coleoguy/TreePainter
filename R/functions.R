bc0154a3ca33bca744093524d3e2effaf1823ff4
Terrence Sylvester <52291097+Tsylvester8@users.noreply.github.com>
2021-05-12 15:38:21 -0500

    Update functions.R

--- R/functions.R
+++ R/functions.R
@@ -8,15 +8,122 @@ treePaintR <- function(tree = NULL,
                        iter.check = NULL,
                        iter.check.interval = NULL){
   # do the checks
-  makeChecks(tree = tree,
-             tip_states = tip_states,
-             qmat = qmat,
-             iter = iter,
-             rate.classes = rate.classes,
-             step = step,
-             verbose = verbose,
-             iter.check = iter.check,
-             iter.check.interval = iter.check.interval)
+  # add checks
+  # checks related to the tree
+  # presence of tree
+  if(is.null(tree)){
+    stop("No imput tree")
+  }else{
+    # class of tree
+    if(class(tree) != "phylo"){
+      stop("tree should class of 'Phylo'")
+    }
+    # bifurcation of tree
+    if(!is.binary(tree)){
+      stop("tree should be stricly bifurcating")
+    }
+    # rootedness
+    if(!is.rooted(tree)){
+      stop("tree should be rooted")
+    }
+  }
+  # checks related to tip states
+  # presence of tip states
+  if(is.null(tip_states)){
+    stop("tip states are missing")
+  }else{
+    # length of tip states should be equal to the number of tips
+    if(length(tip_states) != Ntip(tree)){
+      stop("length of tip states does not match with the number of tips")
+    }
+    # tip states should be integer
+    if(!is.numeric(tip_states)){
+      stop("tip states should integers")
+    }
+    # tip states should start from 1
+    if(min(range(tip_states)) != 1){
+      stop("Starting value of tip states should be 1")
+    }
+  }
+  # checks related to qmat
+  # presence of qmat
+  if(is.null(qmat)){
+    stop("missing Q matrix")
+  }else{
+    if(nrow(qmat) != ncol(qmat)){
+      stop("number of rows and columns in the Q Matrix does not match")
+    }
+    if(any(rowSums(qmat) != 0)){
+      stop(paste("row(s)", which(rowSums(qmat) != 0), "does not add up to zero"))
+    }
+  }
+  # checks related to iterations
+  # presence of iterations
+  if(is.null(iter)){
+    stop("iter is missing")
+  }else{
+    # iter should be single value
+    if(length(iter) > 1){
+      stop("iter should be single numeric value")
+    }
+    # iter should be positive
+    if(iter < 0){
+      stop("iter should be positive")
+    }
+    # iter should be numeric
+    if(!is.numeric(iter)){
+      stop("iter should be of class an integer")
+    }
+    # iter should not contain decimal values
+    if(iter %% 1 != 0){
+      stop("rate classes should not contain decimal values")
+    }
+  }
+  # checks related to rate class
+  # presence of rate class
+  if(is.null(rate.classes)){
+    stop("rate classes are missing")
+  }else{
+    # rate class should be single value
+    if(length(rate.classes) > 1){
+      stop("rate classes should be single numeric value")
+    }
+    # rate class should be positive
+    if(rate.classes < 0){
+      stop("rate class should be positive")
+    }
+    # rate class should be numeric
+    if(!is.numeric(rate.classes)){
+      stop("rate classes should be an integer")
+    }
+    # rate class should not contain decimal values
+    if(rate.classes %% 1 != 0){
+      stop("rate classes should not contain decimal values")
+    }
+  }
+  # checks related to iter check
+  if(is.null(iter.check)){
+    iter.check <- F
+  }else{
+    if(!(is.logical(iter.check))){
+      stop("iter.check should be logical")
+    }
+  }
+  # checks related to iter check interval
+  if(is.null(iter.check.interval)){
+    iter.check.interval <- 1000
+  }else{
+    if(iter.check.interval %% 1 != 0){
+      print("iter.check.interval is not an integer. Setting it to the default value of 1000 generations")
+      iter.check.interval <- 1000
+    }
+  }
+  # iter and iter check interval cannot be same values
+  if(iter.check == T){
+    if(iter == iter.check.interval){
+      stop("iter and iter check interval has to be different values. However the difference can be as small as 1")
+    }
+  }
   
   # set rate classes
   #TODO make this step better
@@ -212,132 +319,3 @@ plot.rateTree <- function(tree, rates,
        edge.width = edge.width)
   par(bg=foo$bg)
 }
-
-
-makeChecks <- function(tree = tree,
-                       tip_states = tip_states,
-                       qmat = qmat,
-                       iter = iter,
-                       rate.classes = rate.classes,
-                       step = step,
-                       verbose = verbose,
-                       iter.check = iter.check,
-                       iter.check.interval = iter.check.interval){
-  # add checks
-  # checks related to the tree
-  # presence of tree
-  if(is.null(tree)){
-    stop("No imput tree")
-  }else{
-    # class of tree
-    if(class(tree) != "phylo"){
-      stop("tree should class of 'Phylo'")
-    }
-    # bifurcation of tree
-    if(!is.binary(tree)){
-      stop("tree should be stricly bifurcating")
-    }
-    # rootedness
-    if(!is.rooted(tree)){
-      stop("tree should be rooted")
-    }
-  }
-  # checks related to tip states
-  # presence of tip states
-  if(is.null(tip_states)){
-    stop("tip states are missing")
-  }else{
-    # length of tip states should be equal to the number of tips
-    if(length(tip_states) != Ntip(tree)){
-      stop("length of tip states does not match with the number of tips")
-    }
-    # tip states should be integer
-    if(!is.numeric(tip_states)){
-      stop("tip states should integers")
-    }
-    # tip states should start from 1
-    if(min(range(tip_states)) != 1){
-      stop("Starting value of tip states should be 1")
-    }
-  }
-  # checks related to qmat
-  # presence of qmat
-  if(is.null(qmat)){
-    stop("missing Q matrix")
-  }else{
-    if(nrow(qmat) != ncol(qmat)){
-      stop("number of rows and columns in the Q Matrix does not match")
-    }
-    if(any(rowSums(qmat) != 0)){
-      stop(paste("row(s)", which(rowSums(qmat) != 0), "does not add up to zero"))
-    }
-  }
-  # checks related to iterations
-  # presence of iterations
-  if(is.null(iter)){
-    stop("iter is missing")
-  }else{
-    # iter should be single value
-    if(length(iter) > 1){
-      stop("iter should be single numeric value")
-    }
-    # iter should be positive
-    if(iter < 0){
-      stop("iter should be positive")
-    }
-    # iter should be numeric
-    if(!is.numeric(iter)){
-      stop("iter should be of class an integer")
-    }
-    # iter should not contain decimal values
-    if(iter %% 1 != 0){
-      stop("rate classes should not contain decimal values")
-    }
-  }
-  # checks related to rate class
-  # presence of rate class
-  if(is.null(rate.classes)){
-    stop("rate classes are missing")
-  }else{
-    # rate class should be single value
-    if(length(rate.classes) > 1){
-      stop("rate classes should be single numeric value")
-    }
-    # rate class should be positive
-    if(rate.classes < 0){
-      stop("rate class should be positive")
-    }
-    # rate class should be numeric
-    if(!is.numeric(rate.classes)){
-      stop("rate classes should be an integer")
-    }
-    # rate class should not contain decimal values
-    if(rate.classes %% 1 != 0){
-      stop("rate classes should not contain decimal values")
-    }
-  }
-  # checks related to iter check
-  if(is.null(iter.check)){
-    iter.check <- F
-  }else{
-    if(!(is.logical(iter.check))){
-      stop("iter.check should be logical")
-    }
-  }
-  # checks related to iter check interval
-  if(is.null(iter.check.interval)){
-    iter.check.interval <- 1000
-  }else{
-    if(iter.check.interval %% 1 != 0){
-      print("iter.check.interval is not an integer. Setting it to the default value of 1000 generations")
-      iter.check.interval <- 1000
-    }
-  }
-  # iter and iter check interval cannot be same values
-  if(iter.check == T){
-    if(iter == iter.check.interval){
-      stop("iter and iter check interval has to be different values. However the difference can be as small as 1")
-    }
-  }
-}
-
